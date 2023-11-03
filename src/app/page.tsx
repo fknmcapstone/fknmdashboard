29d91ca5cee18c94681f42cea5157902c807c691
Ysabelle <ykmiec.info@gmail.com>
2023-11-03 18:49:57 -0400

    First change to resemble a landing page!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,52 +4,51 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <Image
+            src="/fknm_logo.png"
+            alt="FKNM Logo"
+            width={50}
+            height={50}
+            priority
+          />
         </div>
+
+        <p>Feeding Kids, Nourishing Minds</p>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.navigationbar}>
+        <p>Current Programs</p>
+        <p>Research</p>
+        <p>News</p>
+        <p>Publications</p>
+        <p>About FKNM</p>
+      </div>
+
+      <div className={styles.blurb}>
+        <p>Hello</p>
       </div>
 
       <div className={styles.grid}>
+        <p>Find aaaaa in-depth information about Next.js features and API.</p>
+
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find aaaaa in-depth information about Next.js features and API.</p>
+          <h3>Current Programs</h3>
+          <div className={styles.logo}>
+            <Image
+              src="/fknm_logo.png"
+              alt="FKNM Logo"
+              width={50}
+              height={50}
+              priority
+            />
+          </div>
         </a>
 
         <a
@@ -58,36 +57,36 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <h3>Current Programs</h3>
+          <div className={styles.logo}>
+            <Image
+              src="/fknm_logo.png"
+              alt="FKNM Logo"
+              width={50}
+              height={50}
+              priority
+            />
+          </div>
         </a>
+        <p>Find aaaaa in-depth information about Next.js features and API.</p>
 
+        <p>Find aaaaa in-depth information about Next.js features and API.</p>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <h3>Current Programs</h3>
+          <div className={styles.logo}>
+            <Image
+              src="/fknm_logo.png"
+              alt="FKNM Logo"
+              width={50}
+              height={50}
+              priority
+            />
+          </div>
         </a>
       </div>
     </main>
