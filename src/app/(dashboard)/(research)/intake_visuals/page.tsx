61443ca9178e79d8b1ab3a58f9305aeca05427d2
Ysabelle <ykmiec.info@gmail.com>
2024-01-28 15:27:54 -0500

    Intake Visuals updated. Added return to top button and loops for charts and shortcut menu

--- src/app/(dashboard)/(research)/intake_visuals/page.tsx
+++ src/app/(dashboard)/(research)/intake_visuals/page.tsx
@@ -1,85 +1,71 @@
 import styles from "./page.module.css";
 import Link from "next/link";
+import ToTopButton from "./reveal";
 
-let NUM_CHARTS = 6;
+/**
+ * Likely this could be good in a map, with category names being keys
+ * and a list of the associated chartnames as the value
+ */
+
+const categoryNames = ["Category 1", "Category 2", "Category 3"];
+
+const chartNames = Array.from({ length: 10 }, (value, index) => index).map(
+  (index) => {
+    return "Data Visual " + index;
+  }
+);
+
+let NUM_CHARTS = categoryNames.length * chartNames.length;
+
+function shortcutMenuList() {
+  let shortcuts = [];
+  for (var category of categoryNames) {
+    shortcuts.push(
+      <p className={styles.shortcutCategoryHeader}>{category}</p>,
+      <div className={styles.shortcutMenuSeparator} />
+    );
+    for (var chartName of chartNames) {
+      shortcuts.push(
+        <Link href={"#" + category + chartName}>{chartName}</Link>
+      );
+    }
+
+    shortcuts.push(<p className={styles.spacing}></p>);
+  }
+  return shortcuts;
+}
+
+function chartList() {
+  let charts = [];
+  for (var category of categoryNames) {
+    charts.push(
+      <div className={styles.chartCategoryHeader}>{category}</div>,
+      <div className={styles.shortcutMenuSeparator} />
+    );
+    for (var chartName of chartNames) {
+      charts.push(
+        <div id={category + chartName} className={styles.chartArea}>
+          <div className={styles.tooltip}></div>
+        </div>
+      );
+    }
+  }
+  return charts;
+}
 
 export default function IntakeVisuals() {
   return (
     <main className={styles.main}>
+      <ToTopButton />
+
       <div
         className={styles.fixedMenuContainer}
-        style={{ height: 610 * NUM_CHARTS }}
+        style={{ height: 33.8 * NUM_CHARTS + "rem" }}
       >
-        <div className={styles.shortcutMenu}>
-          <p className={styles.shortcutCategoryHeader}>Category 1</p>
-          <div className={styles.shortcutMenuSeparator} />
-          <p>Data Visual 1</p>
-          <p>Data Visual 2</p>
-          <p className={styles.spacing} />
-          <p className={styles.shortcutCategoryHeader}>Category 2</p>
-          <div className={styles.shortcutMenuSeparator} />
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-          <p>Data Visual 4</p>
-          <p>Data Visual 3</p>
-        </div>
+        <div className={styles.shortcutMenu}>{shortcutMenuList()}</div>
       </div>
 
-      <div className={styles.chartColumn}>
-        <div className={styles.chartCategoryHeader}>Category 1</div>
-        <div className={styles.shortcutMenuSeparator} />
-        <div className={styles.chartArea}>
-          <div className={styles.tooltip}></div>
-        </div>
-        <div className={styles.chartArea}>
-          <div className={styles.tooltip}></div>
-        </div>
-        <div className={styles.chartArea}>
-          <div className={styles.tooltip}></div>
-        </div>
-      </div>
-      <div className={styles.chartColumn}>
-        <div className={styles.chartCategoryHeader}>Category 2</div>
-        <div className={styles.shortcutMenuSeparator} />
-        <div className={styles.chartArea}>
-          <div className={styles.tooltip}></div>
-        </div>
-        <div className={styles.chartArea}>
-          <div className={styles.tooltip}></div>
-        </div>
-        <div className={styles.chartArea}>
-          <div className={styles.tooltip}></div>
-        </div>
-      </div>
+      <div className={styles.chartColumn}>{chartList()}</div>
     </main>
   );
 }
---
+++ src/app/(dashboard)/(research)/intake_visuals/reveal.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+
+const ToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false);
+    };
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    isVisible &&
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+  };
+
+  return (
+    <button
+      className={styles.returnToTopButton}
+      style={{ display: isVisible ? "block" : "none" }}
+      onClick={scrollToTop}
+    >
+      <svg viewBox="-8 -8 40 40" fill="none">
+        <path
+          d="M17.6568 8.96219L16.2393 10.3731L12.9843 7.10285L12.9706 20.7079L10.9706 20.7059L10.9843 7.13806L7.75404 10.3532L6.34314 8.93572L12.0132 3.29211L17.6568 8.96219Z"
+          fill="#626262"
+          stroke="#626262"
+          strokeWidth="1"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default ToTopButton;
